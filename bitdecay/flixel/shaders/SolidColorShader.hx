76e3acbae5e57a7f752c26dd3bb4284ef89f6b73
Logan <monday.hopscotch@gmail.com>
2024-05-07 13:58:47 -0600

    add shaders stripped from the template project

---
+++ bitdecay/flixel/shaders/SolidColorShader.hx
@@ -0,0 +1,34 @@
+package bitdecay.flixel.shaders;
+
+import flixel.util.FlxColor;
+import openfl.display.ShaderParameter;
+import flixel.system.FlxAssets.FlxShader;
+
+// Allows setting a sprite to be a solid color, such as blinking white while taking damage
+class SolidColorShader extends FlxShader {
+	@:glFragmentSource('
+        #pragma header
+
+		uniform vec4 color;
+        uniform bool isShaderActive;
+
+        void main()
+        {
+            vec4 pixel = texture2D(bitmap, openfl_TextureCoordv);
+
+			if (!isShaderActive)
+			{
+				gl_FragColor = pixel;
+				return;
+            } 
+
+            if (pixel.a != 0.0) {
+                gl_FragColor = color;
+            }
+        }')
+	public function new(color:FlxColor) {
+		super();
+
+		this.color.value = [color.red, color.green, color.blue, color.alpha];
+	}
+}
