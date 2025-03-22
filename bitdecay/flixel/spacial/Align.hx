699a353e61cd5b215f8078b797efc531f0d7f236
Logan <monday.hopscotch@gmail.com>
2025-03-22 17:06:25 -0600

    add edge alignment helper

--- bitdecay/flixel/spacial/Align.hx
+++ bitdecay/flixel/spacial/Align.hx
@@ -1,5 +1,8 @@
 package bitdecay.flixel.spacial;
 
+import flixel.FlxSprite;
+import bitdecay.flixel.system.QuickLog;
+import flixel.util.FlxDirectionFlags;
 import flixel.math.FlxPoint;
 import flixel.util.FlxAxes;
 import flixel.FlxObject;
@@ -26,6 +29,47 @@ class Align {
 				a.setPositionMidpoint(pA.x, pB.y);
 			default:
 		}
-	
 	}
+
+	/**
+	 * Aligns `a` to `b` on the provided edges
+	**/
+	public static function edge(a:FlxSprite, b:FlxSprite, edges:FlxDirectionFlags, feature:Feature=HITBOX) {
+		if (edges.has(LEFT.with(RIGHT)) || edges.has(UP.with(DOWN))) {
+			QuickLog.error('cannot align object edges on opposite sides simultaneously');
+			return;
+		}
+
+		if (edges == NONE) {
+			QuickLog.notice('attempting to align on no edges');
+			return;
+		}
+
+		if (edges.has(UP)) {
+			a.y = b.y;
+		} else if (edges.has(DOWN)) {
+			a.y = b.y + b.height - a.height;
+		}
+		
+		if (edges.has(LEFT)) {
+			a.x = b.x - b.offset.x + a.offset.x;
+		} else if (edges.has(RIGHT)) {
+			a.x = b.x + b.width - b.offset.x - a.width + a.offset.x;
+		}
+
+		if (feature == HITBOX_WITH_OFFSET) {
+			if (edges.hasAny(UP.with(DOWN))) {
+				a.y = a.y - b.offset.y + a.offset.y;
+			}
+
+			if (edges.hasAny(LEFT.with(RIGHT))) {
+				a.x = a.x - b.offset.x + a.offset.x;
+			}
+		}
+	}
+}
+
+enum Feature {
+	HITBOX;
+	HITBOX_WITH_OFFSET;
 }
