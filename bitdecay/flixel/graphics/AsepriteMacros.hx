4a460bbe6db986c05f0553d6614f053c5d4f0e21
Logan <monday.hopscotch@gmail.com>
2025-04-21 23:20:33 -0600

    fix import paths

--- bitdecay/flixel/graphics/AsepriteMacros.hx
+++ bitdecay/flixel/graphics/AsepriteMacros.hx
@@ -8,10 +8,10 @@ class AsepriteMacros {
 	public static macro function tagNames(path:String) {
 		return try {
 			var json = haxe.Json.parse(sys.io.File.getContent(path));
-			var tags:Array<loaders.AsepriteTypes.AseAtlasTag> = json.meta.frameTags;
+			var tags:Array<bitdecay.flixel.graphics.AsepriteTypes.AseAtlasTag> = json.meta.frameTags;
 			var map:Dynamic = {};
 			if (tags.length == 0) {
-				var frames:Array<loaders.AsepriteTypes.AseAtlasFrame> = json.frames;
+				var frames:Array<bitdecay.flixel.graphics.AsepriteTypes.AseAtlasFrame> = json.frames;
 				for (frame in frames) {
 					Reflect.setField(map, clean(frame.filename), frame.filename);
 				}
@@ -33,7 +33,7 @@ class AsepriteMacros {
 	public static macro function sliceNames(path:String) {
 		return try {
 			var json = haxe.Json.parse(sys.io.File.getContent(path));
-			var slices:Array<loaders.AsepriteTypes.AseAtlasSlice> = json.meta.slices;
+			var slices:Array<bitdecay.flixel.graphics.AsepriteTypes.AseAtlasSlice> = json.meta.slices;
 			var map:Dynamic = {};
 			for (s in slices) {
 				for (key in s.keys) {
@@ -51,8 +51,8 @@ class AsepriteMacros {
 	**/ 
 	public static macro function layerNames(path:String) {
 		return try {
-			var atlas:loaders.AsepriteTypes.AseAtlas = haxe.Json.parse(sys.io.File.getContent(path));
-			var layers:Array<loaders.AsepriteTypes.AseAtlasLayer> = atlas.meta.layers;
+			var atlas:bitdecay.flixel.graphics.AsepriteTypes.AseAtlas = haxe.Json.parse(sys.io.File.getContent(path));
+			var layers:Array<bitdecay.flixel.graphics.AsepriteTypes.AseAtlasLayer> = atlas.meta.layers;
 			var map:Dynamic = {};
 			for (l in layers) {
 				Reflect.setField(map, clean('${l.name}'), '${l.name}');
@@ -65,8 +65,8 @@ class AsepriteMacros {
 
 	public static macro function frameUserData(path:String, layer:String) {
 		return try {
-			var atlas:loaders.AsepriteTypes.AseAtlas = haxe.Json.parse(sys.io.File.getContent(path));
-			var layers:Array<loaders.AsepriteTypes.AseAtlasLayer> = atlas.meta.layers;
+			var atlas:bitdecay.flixel.graphics.AsepriteTypes.AseAtlas = haxe.Json.parse(sys.io.File.getContent(path));
+			var layers:Array<bitdecay.flixel.graphics.AsepriteTypes.AseAtlasLayer> = atlas.meta.layers;
 			var map:Map<Int, String> = [];
 			for (l in layers) {
 				if (l.name == layer) {
